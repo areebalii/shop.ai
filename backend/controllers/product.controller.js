bef294af5bcd0a6caeb8213c4ccfbd162082bc6e
areebalii <areebktk8@gmail.com>
2026-02-06 18:19:25 +0500

    Add product management functions: add, list, single, and remove

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,12 @@
+
+// function to add products
+export const addProduct = async (req, res) => {}
+
+// function for list products
+export const listProducts = async (req, res) => {}
+
+// function to get single product info
+export const singleProduct = async (req, res) => {}
+
+// function to remove product details
+export const removeProduct = async (req, res) => {}
