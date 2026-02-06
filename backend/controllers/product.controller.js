61675334a7e3fbf22d4b161313e81e9f2bf47da8
areebalii <areebktk8@gmail.com>
2026-02-06 20:44:50 +0500

    Implement product management routes and add product functionality with file uploads

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,6 +1,23 @@
 
 // function to add products
-export const addProduct = async (req, res) => {}
+export const addProduct = async (req, res) => {
+  console.log("Content-Type:", req.headers['content-type']);
+  console.log("Raw Body:", req.body);
+  console.log("All Files:", req.files);
+  try {
+    const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
+
+    const image1 = req.files.image1[0]
+    const image2 = req.files.image2[0]
+    const image3 = req.files.image3[0]
+    const image4 = req.files.image4[0]
+    console.log(name, description, price , category, subCategory, sizes, bestSeller)
+    console.log(image1, image2, image3, image4)
+    res.json({})
+  } catch (error) {
+    res.json( { success: false, message: error.message } )
+  }
+}
 
 // function for list products
 export const listProducts = async (req, res) => {}
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/user.route.js'
+import productRouter from './routes/product.route.js'
 
 // App Config
 const app = express()
@@ -20,6 +21,7 @@ app.use(cors())
 
 // api endpoints
 app.use("/api/user", userRouter)
+app.use("/api/product", productRouter)
 
 app.listen(port,()=>{
     console.log(`Server is running on port http://localhost:${port}`)
