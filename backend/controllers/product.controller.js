767ec17bfb50543379fe05c9ef1c93811528b133
areebalii <areebktk8@gmail.com>
2026-02-07 20:06:36 +0500

    Fix image file handling in addProduct function and clean up formatting

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -7,23 +7,23 @@ export const addProduct = async (req, res) => {
   try {
     const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
 
-    const image1 = req.files.image1[0]
-    const image2 = req.files.image2[0]
-    const image3 = req.files.image3[0]
-    const image4 = req.files.image4[0]
-    console.log(name, description, price , category, subCategory, sizes, bestSeller)
+    const image2 = req.files.image1 && req.files.image2[0]
+    const image1 = req.files.image2 && req.files.image1[0]
+    const image3 = req.files.image3 && req.files.image3[0]
+    const image4 = req.files.image4 && req.files.image4[0]
+    console.log(name, description, price, category, subCategory, sizes, bestSeller)
     console.log(image1, image2, image3, image4)
     res.json({})
   } catch (error) {
-    res.json( { success: false, message: error.message } )
+    res.json({ success: false, message: error.message })
   }
 }
 
 // function for list products
-export const listProducts = async (req, res) => {}
+export const listProducts = async (req, res) => { }
 
 // function to get single product info
-export const singleProduct = async (req, res) => {}
+export const singleProduct = async (req, res) => { }
 
 // function to remove product details
-export const removeProduct = async (req, res) => {}
+export const removeProduct = async (req, res) => { }
