14e0a0376219f80a359d28736457f797e38f25b8
areebalii <areebktk8@gmail.com>
2026-02-10 07:44:22 +0500

    Enhance product management: update image upload path, implement error handling, and complete list, single, and remove product functions

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -15,7 +15,7 @@ export const addProduct = async (req, res) => {
     const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
     const imagesUrl = await Promise.all(
       images.map(async (item) => {
-        const result = await cloudinary.uploader.upload(item.path, { resource_type: "image" });
+        const result = await cloudinary.uploader.upload(item.path, { resource_type: "image", folder: "ecommerce/products" });
         return result.secure_url;
       })
     );
@@ -42,10 +42,55 @@ export const addProduct = async (req, res) => {
 }
 
 // function for list products
-export const listProducts = async (req, res) => { }
+export const listProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(new ApiResponse(200, products, "Products fetched successfully"));
+  } catch (error) {
+    res.json({ success: false, message: error.message })
+  }
+ }
 
 // function to get single product info
-export const singleProduct = async (req, res) => { }
+export const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+
+    if (!productId) {
+      return res.status(400).json(new ApiResponse(400, null, "Product ID is required"));
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json(new ApiResponse(404, null, "Product not found"));
+    }
+
+    return res.status(200).json(new ApiResponse(200, product, "Product fetched successfully"));
+  } catch (error) {
+    
+  }
+ }
 
 // function to remove product details
-export const removeProduct = async (req, res) => { }
+export const removeProduct = async (req, res) => {
+  try {
+    const { id } = req.body; 
+
+    if (!id) {
+      return res.status(400).json(new ApiResponse(400, null, "Product ID is required"));
+    }
+
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json(new ApiResponse(404, null, "Product not found"));
+    }
+
+    return res.status(200).json(new ApiResponse(200, null, "Product removed successfully"));
+
+  } catch (error) {
+    console.error("Delete Error:", error);
+    return res.status(500).json(new ApiResponse(500, null, error.message));
+  }
+};
