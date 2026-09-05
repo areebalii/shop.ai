312144096cc6f41f9672c8e58e4a7c57b51437ac
areebalii <areebktk8@gmail.com>
2026-09-05 13:36:08 +0500

    feat: update server.js to conditionally start the server in non-production environments and export app; add vercel.json for deployment configuratiom

--- backend/server.js
+++ backend/server.js
@@ -118,6 +118,10 @@ app.get('/', (req, res) => {
     res.send('API is running!')
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+export default app;
